6960eba992e0cb15d8a10c466d87b8a1d91fb9a5
Ivan Tkachuk <ivtkac@gmail.com>
2025-11-24 19:20:51 +0200

    Make state file stores in S3 bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 terraform {
+  backend "s3" {
+    bucket = "gearxed-tf-state"
+    key = "bootstrap/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
-        source = "hashicorp/aws",
-        version = "~> 3.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
@@ -11,7 +19,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-011899242bb902164"
-    instance_type = "t2.micro"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "gearxed-tf-state"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
